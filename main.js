d193334274bb54a0c160425c97ede58a99e82416
seanwangelin <seanwangelin@outlook.com>
2022-12-28 20:50:29 -0600

    added getWinner function and cleaned code

--- main.js
+++ main.js
@@ -5,47 +5,50 @@ let board = document.getElementById('board');
 let player = 'x';
 
 let gameBoard = [
-    '','','',
-    '','','',
-    '','',''
+    null,null,null,
+    null,null,null,
+    null,null,null
 ];
 
-let winningCombos= [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-
-    [0,4,8],
-    [2,4,6]
-];
-
-// for(let i=0; i<winningCombos.length; i++){
-//     console.log(winningCombos[i]);
-//     for(let j=0; j<winningCombos[i].length; j++){
-//         console.log(winningCombos[i][j]);
-//     }
-// }
+const getWinner = symbol => {
+    if(gameBoard[0]===symbol && gameBoard[1]===symbol && gameBoard[2]===symbol){
+        console.log('winner');
+    } else if(gameBoard[3]===symbol && gameBoard[4]===symbol && gameBoard[5]===symbol){
+        console.log('winner');
+    } else if(gameBoard[6]===symbol && gameBoard[7]===symbol && gameBoard[8]===symbol){
+        console.log('winner');
+    } else if(gameBoard[0]===symbol && gameBoard[3]===symbol && gameBoard[6]===symbol){
+        console.log('winner');
+    } else if(gameBoard[1]===symbol && gameBoard[4]===symbol && gameBoard[7]===symbol){
+        console.log('winner');
+    } else if(gameBoard[2]===symbol && gameBoard[5]===symbol && gameBoard[8]===symbol){
+        console.log('winner');
+    } else if(gameBoard[0]===symbol && gameBoard[4]===symbol && gameBoard[8]===symbol){
+        console.log('winner');
+    } else if(gameBoard[2]===symbol && gameBoard[4]===symbol && gameBoard[6]===symbol){
+        console.log('winner');
+    }
+}
 
 boardSquares.forEach(val=>{
-
+    
     val.addEventListener('click', function(){
         if(player === 'x'){
             val.innerHTML='x';
             gameBoard[val.id]='x';
+            getWinner(player);
+            
             player='y';
         } else {
             val.innerHTML='y';
             gameBoard[val.id]='y';
+            getWinner(player);
+
             player='x';
         }
+        
+        
+
         console.log(gameBoard);
     })
 });
-
-
-// console.log(boardSquare);
-console.log(gameBoard[0]);
