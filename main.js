03c93c9c8f3e00748bcb34fdd3a868c6a420baa3
Sean Wangelin <seanwangelin@outlook.com>
2023-01-04 20:31:50 -0600

    cleaned up

--- main.js
+++ main.js
@@ -8,117 +8,91 @@ const multiPlayer = document.getElementById('multiPlayer')
 
 let player = 'x';
 
-let gameMode=2;
+let gameMode = 2;
 
 let gameBoard = [
-    null,null,null,
-    null,null,null,
-    null,null,null
+    null, null, null,
+    null, null, null,
+    null, null, null
 ];
 
 const getWinner = symbol => {
-    if(gameBoard[0]===symbol && gameBoard[1]===symbol && gameBoard[2]===symbol){
+    if (gameBoard[0] === symbol && gameBoard[1] === symbol && gameBoard[2] === symbol) {
         console.log('winner');
-    } else if(gameBoard[3]===symbol && gameBoard[4]===symbol && gameBoard[5]===symbol){
+    } else if (gameBoard[3] === symbol && gameBoard[4] === symbol && gameBoard[5] === symbol) {
         console.log('winner');
-    } else if(gameBoard[6]===symbol && gameBoard[7]===symbol && gameBoard[8]===symbol){
+    } else if (gameBoard[6] === symbol && gameBoard[7] === symbol && gameBoard[8] === symbol) {
         console.log('winner');
-    } else if(gameBoard[0]===symbol && gameBoard[3]===symbol && gameBoard[6]===symbol){
+    } else if (gameBoard[0] === symbol && gameBoard[3] === symbol && gameBoard[6] === symbol) {
         console.log('winner');
-    } else if(gameBoard[1]===symbol && gameBoard[4]===symbol && gameBoard[7]===symbol){
+    } else if (gameBoard[1] === symbol && gameBoard[4] === symbol && gameBoard[7] === symbol) {
         console.log('winner');
-    } else if(gameBoard[2]===symbol && gameBoard[5]===symbol && gameBoard[8]===symbol){
+    } else if (gameBoard[2] === symbol && gameBoard[5] === symbol && gameBoard[8] === symbol) {
         console.log('winner');
-    } else if(gameBoard[0]===symbol && gameBoard[4]===symbol && gameBoard[8]===symbol){
+    } else if (gameBoard[0] === symbol && gameBoard[4] === symbol && gameBoard[8] === symbol) {
         console.log('winner');
-    } else if(gameBoard[2]===symbol && gameBoard[4]===symbol && gameBoard[6]===symbol){
+    } else if (gameBoard[2] === symbol && gameBoard[4] === symbol && gameBoard[6] === symbol) {
         console.log('winner');
     }
 }
 
-let firstNull = gameBoard.find(element =>{
+let firstNull = gameBoard.find(element => {
     return element === null;
-    }
+}
 );
 
-// let newBoardSquares = Array.from(boardSquares);
-
-// console.log(newBoardSquares);
 console.log(firstNull)
 
-singlePlayer.addEventListener('click', function(){
-    gameMode=1;
-    boardSquares.forEach(val=>{
-        val.addEventListener('click', function(){
-            if(val.innerHTML==='x'||val.innerHTML==='y'){
+singlePlayer.addEventListener('click', function () {
+    gameMode = 1;
+    boardSquares.forEach(val => {
+        val.addEventListener('click', function () {
+            if (val.innerHTML === 'x' || val.innerHTML === 'y') {
                 console.log("you can't do that")
             } else {
-            val.innerHTML='x';
-            gameBoard[val.id]='x';
-            for(let i=gameBoard.length-1; i>=0; i--){
-                if(gameBoard[i]===null){
-                    let computerSquare = document.getElementById(i);
-                    computerSquare.innerHTML = 'y';
-                    gameBoard[i]='y';
-                    break;
+                val.innerHTML = 'x';
+                gameBoard[val.id] = 'x';
+                for (let i = gameBoard.length - 1; i >= 0; i--) {
+                    if (gameBoard[i] === null) {
+                        let computerSquare = document.getElementById(i);
+                        computerSquare.innerHTML = 'y';
+                        gameBoard[i] = 'y';
+                        break;
+                    }
                 }
-            }
-            console.log(gameBoard);
-            getWinner(player);
+                console.log(gameBoard);
+                getWinner(player);
             }
         })
-        
+
     })
     console.log('single');
 })
-// boardSquares.forEach(val=>{
-//     console.log(val);
-// })
-multiPlayer.addEventListener('click', function(){
-    gameMode=2;
-    boardSquares.forEach(val=>{
 
-        val.addEventListener('click', function(){
-            if(player === 'x'){
-                val.innerHTML='x';
-                gameBoard[val.id]='x';
-                getWinner(player);
-
-                player='y';
+multiPlayer.addEventListener('click', () => {
+    gameMode = 2;
+    boardSquares.forEach(val => {
+        val.addEventListener('click', () => {
+            if (val.innerHTML === 'x' || val.innerHTML === 'y') {
+                console.log("you can't do that")
             } else {
-                val.innerHTML='y';
-                gameBoard[val.id]='y';
-                getWinner(player);
+                if (player === 'x') {
+                    val.innerHTML = 'x';
+                    gameBoard[val.id] = 'x';
+                    getWinner(player);
 
-                player='x';
+                    player = 'y';
+                } else {
+                    val.innerHTML = 'y';
+                    gameBoard[val.id] = 'y';
+                    getWinner(player);
+                    player = 'x';
+                }
+
+
+                console.log(gameBoard);
             }
-
-            console.log(gameBoard);
         })
     });
 })
 
-// if(gameMode===2){
-//     boardSquares.forEach(val=>{
-
-//         val.addEventListener('click', function(){
-//             if(player === 'x'){
-//                 val.innerHTML='x';
-//                 gameBoard[val.id]='x';
-//                 getWinner(player);
-
-//                 player='y';
-//             } else {
-//                 val.innerHTML='y';
-//                 gameBoard[val.id]='y';
-//                 getWinner(player);
-
-//                 player='x';
-//             }
-
-//             console.log(gameBoard);
-//         })
-//     });
-// } else if(gameMode===1){
-//     console.log('singlePlayer')
-// }
