6f19148760c3b5dc7b680ff3b50f83786d93e98a
Sean Wangelin <seanwangelin@outlook.com>
2023-01-05 20:35:01 -0600

    can't play anymore logic

--- main.js
+++ main.js
@@ -10,21 +10,54 @@ let player = 'x';
 
 let gameMode = 2;
 
+let winner = false;
+
 let gameBoard = [
     null, null, null,
     null, null, null,
     null, null, null
 ];
 
+const getWinner = symbol => {
+
+    if (gameBoard[0] === symbol && gameBoard[1] === symbol && gameBoard[2] === symbol) {
+        winner=true;
+    } else if (gameBoard[3] === symbol && gameBoard[4] === symbol && gameBoard[5] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[6] === symbol && gameBoard[7] === symbol && gameBoard[8] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[0] === symbol && gameBoard[3] === symbol && gameBoard[6] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[1] === symbol && gameBoard[4] === symbol && gameBoard[7] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[2] === symbol && gameBoard[5] === symbol && gameBoard[8] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[0] === symbol && gameBoard[4] === symbol && gameBoard[8] === symbol) {
+        console.log('winner');
+        winner=true;
+    } else if (gameBoard[2] === symbol && gameBoard[4] === symbol && gameBoard[6] === symbol) {
+        console.log('winner');
+        winner=true;
+    }
+}
+
 const singlePlayerFunc = () =>{
+
     boardSquares.forEach(val => {
-        val.addEventListener('click', function () {
-            if (val.innerHTML === 'x' || val.innerHTML === 'y') {
+        const singleLogic = () => {
+            if(val.innerHTML === 'x' || val.innerHTML === 'y') {
                 console.log("you can't do that")
+            } else if(winner) {
+                console.log(`can't play anymore`)
             } else {
                 val.innerHTML = 'x';
                 gameBoard[val.id] = 'x';
-
+                
                 for (let i = gameBoard.length - 1; i >= 0; i--) {
                     if (gameBoard[i] === null) {
                         let computerSquare = document.getElementById(i);
@@ -36,12 +69,14 @@ const singlePlayerFunc = () =>{
                 console.log(gameBoard);
                 getWinner(player);
             }
-        })
-
+            
+        }
+        val.addEventListener('click', singleLogic);
     })
     singlePlayer.removeEventListener('click',singlePlayerFunc);
     multiPlayer.removeEventListener('click',multiPlayerFunc);
 }
+
 
 const multiPlayerFunc = () =>{
     boardSquares.forEach(val => {
@@ -66,40 +101,13 @@ const multiPlayerFunc = () =>{
             }
         })
     });
-    
+
     singlePlayer.removeEventListener('click',singlePlayerFunc);
     multiPlayer.removeEventListener('click', multiPlayerFunc);
 }
 
-const getWinner = symbol => {
-    if (gameBoard[0] === symbol && gameBoard[1] === symbol && gameBoard[2] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[3] === symbol && gameBoard[4] === symbol && gameBoard[5] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[6] === symbol && gameBoard[7] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[0] === symbol && gameBoard[3] === symbol && gameBoard[6] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[1] === symbol && gameBoard[4] === symbol && gameBoard[7] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[2] === symbol && gameBoard[5] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[0] === symbol && gameBoard[4] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
-    } else if (gameBoard[2] === symbol && gameBoard[4] === symbol && gameBoard[6] === symbol) {
-        console.log('winner');
-    }
-}
-
-let firstNull = gameBoard.find(element => {
-    return element === null;
-}
-);
-
-console.log(firstNull)
 
 singlePlayer.addEventListener('click', singlePlayerFunc)
 
 
 multiPlayer.addEventListener('click', multiPlayerFunc)
-
