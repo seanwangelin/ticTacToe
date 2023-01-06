f229d7a1a872347973b22a3b012487eff8ad877d
Sean Wangelin <seanwangelin@outlook.com>
2023-01-05 21:26:08 -0600

    created message board in lieu of console logs

--- main.js
+++ main.js
@@ -4,11 +4,17 @@ const board = document.getElementById('board');
 
 const singlePlayer = document.getElementById('singlePlayer');
 
-const multiPlayer = document.getElementById('multiPlayer')
+const multiPlayer = document.getElementById('multiPlayer');
+
+const messageBoard = document.getElementById('messageBoard');
 
 let player = 'x';
 
 let gameMode = 2;
+
+let name1='';
+
+let name2='';
 
 let winner = false;
 
@@ -22,41 +28,40 @@ const getWinner = symbol => {
 
     if (gameBoard[0] === symbol && gameBoard[1] === symbol && gameBoard[2] === symbol) {
         winner=true;
-    } else if (gameBoard[3] === symbol && gameBoard[4] === symbol && gameBoard[5] === symbol) {
-        console.log('winner');
-        winner=true;
-    } else if (gameBoard[6] === symbol && gameBoard[7] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
-        winner=true;
-    } else if (gameBoard[0] === symbol && gameBoard[3] === symbol && gameBoard[6] === symbol) {
-        console.log('winner');
-        winner=true;
-    } else if (gameBoard[1] === symbol && gameBoard[4] === symbol && gameBoard[7] === symbol) {
-        console.log('winner');
-        winner=true;
-    } else if (gameBoard[2] === symbol && gameBoard[5] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
-        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
+    } else if (gameBoard[3] === symbol && gameBoard[4] === symbol && gameBoard[5] === symbol) {        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
+    } else if (gameBoard[6] === symbol && gameBoard[7] === symbol && gameBoard[8] === symbol) {        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
+    } else if (gameBoard[0] === symbol && gameBoard[3] === symbol && gameBoard[6] === symbol) {        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
+    } else if (gameBoard[1] === symbol && gameBoard[4] === symbol && gameBoard[7] === symbol) {        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
+    } else if (gameBoard[2] === symbol && gameBoard[5] === symbol && gameBoard[8] === symbol) {        winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
     } else if (gameBoard[0] === symbol && gameBoard[4] === symbol && gameBoard[8] === symbol) {
-        console.log('winner');
         winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
     } else if (gameBoard[2] === symbol && gameBoard[4] === symbol && gameBoard[6] === symbol) {
-        console.log('winner');
         winner=true;
+        messageBoard.innerHTML=`${symbol} wins!!!`;
     }
 }
 
 const singlePlayerFunc = () =>{
     let getName = window.prompt('Enter your name:');
-    document.getElementById('x').innerHTML=`x: ${getName}`;
-    document.getElementById('y').innerHTML=`y: computer`
+    name1 = getName;
+    name2 = 'computer';
+    document.getElementById('x').innerHTML=`x: ${name1}`;
+    document.getElementById('y').innerHTML=`y: ${name2}`;
 
     boardSquares.forEach(val => {
         const singleLogic = () => {
+
             if(val.innerHTML === 'x' || val.innerHTML === 'y') {
-                console.log("you can't do that")
+                messageBoard.innerHTML=`you can't do that!!`
             } else if(winner) {
-                console.log(`can't play anymore`)
+                messageBoard.innerHTML=`The game is over, reset the board!`
             } else {
                 val.innerHTML = 'x';
                 gameBoard[val.id] = 'x';
@@ -64,18 +69,22 @@ const singlePlayerFunc = () =>{
                 for (let i = gameBoard.length - 1; i >= 0; i--) {
                     if (gameBoard[i] === null) {
                         let computerSquare = document.getElementById(i);
+
                         computerSquare.innerHTML = 'y';
                         gameBoard[i] = 'y';
                         break;
                     }
                 }
-                console.log(gameBoard);
+
                 getWinner(player);
             }
             
         }
+
         val.addEventListener('click', singleLogic);
+        messageBoard.innerHTML=`Defeat the computer!`;
     });
+
     singlePlayer.removeEventListener('click',singlePlayerFunc);
     multiPlayer.removeEventListener('click',multiPlayerFunc);
 }
@@ -88,27 +97,33 @@ const multiPlayerFunc = () =>{
     document.getElementById('x').innerHTML=`x: ${getName1}`;
     document.getElementById('y').innerHTML=`y: ${getName2}`;
 
+    name1=getName1;
+    name2=getName2;
+    messageBoard.innerHTML=`${name1}'s turn!`
+
     boardSquares.forEach(val => {
         val.addEventListener('click', () => {
             if (val.innerHTML === 'x' || val.innerHTML === 'y') {
-                console.log("you can't do that")
+                messageBoard.innerHTML=`You can't do that!`;
+
             } else if(winner){
-                console.log(`you can't do that`)
+                messageBoard.innerHTML=`The game is over, reset the board!`
+
             } else {
                 if (player === 'x') {
                     val.innerHTML = 'x';
+                    messageBoard.innerHTML=`${name2}'s turn!`;
                     gameBoard[val.id] = 'x';
                     getWinner(player);
-
                     player = 'y';
+
                 } else {
                     val.innerHTML = 'y';
+                    messageBoard.innerHTML=`${name1}'s turn!`;
                     gameBoard[val.id] = 'y';
                     getWinner(player);
                     player = 'x';
                 }
-
-                console.log(gameBoard);
             }
         })
     });
